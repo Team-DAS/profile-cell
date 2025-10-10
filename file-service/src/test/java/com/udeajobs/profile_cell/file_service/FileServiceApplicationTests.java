ea06ea857a7d44d887873c0e18fb0d73c5a7ffa0
Arge2004 <argenismedina0424@gmail.com>
2025-10-10 16:39:06 -0500

    add file service with Docker configuration and Gradle setup

---
+++ file-service/src/test/java/com/udeajobs/profile_cell/file_service/FileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udeajobs.profile_cell.file_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
